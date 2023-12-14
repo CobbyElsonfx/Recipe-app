3024d5b45c5aed2ccf01a7680d494a1c6aed2993
José Berbesi <88105477+jlberbesi@users.noreply.github.com>
2023-12-14 16:43:36 -0500

    Linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-    has_many :recipes, dependent: :destroy
-    has_many :foods, dependent: :destroy
-  
-    validates :name, presence: true
-  end
+  has_many :recipes, dependent: :destroy
+  has_many :foods, dependent: :destroy
+
+  validates :name, presence: true
+end
