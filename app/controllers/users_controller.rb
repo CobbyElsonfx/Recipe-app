9abe23faf48f0f2f804b2796dd453a4433f6c131
José Berbesi <88105477+jlberbesi@users.noreply.github.com>
2023-12-14 19:43:03 -0500

    Linters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-    before_action :authenticate_user!, except: %i[new create]
-  
-    def show
-      @user = User.find(params[:id])
-    end
+  before_action :authenticate_user!, except: %i[new create]
+
+  def show
+    @user = User.find(params[:id])
   end
+end
